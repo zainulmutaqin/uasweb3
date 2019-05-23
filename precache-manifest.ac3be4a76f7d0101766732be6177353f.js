0a765dff6b9d0edfeb15d2aee9d5cbeb7d32e9d0
zainulmutaqin <zainulmutaqin15@gmail.com>
2019-05-23 11:10:45 +0700

    Updates

---
+++ precache-manifest.ac3be4a76f7d0101766732be6177353f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f9e6565e0fa9cc3ddbfa2334f47725",
+    "url": "/uasweb3/index.html"
+  },
+  {
+    "revision": "f646b35afa71159d75f3",
+    "url": "/uasweb3/static/css/main.8edfd715.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/uasweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f646b35afa71159d75f3",
+    "url": "/uasweb3/static/js/main.d82ae3e1.chunk.js"
+  },
+  {
+    "revision": "fb022459ee158e3fced4",
+    "url": "/uasweb3/static/js/runtime~main.80a0655b.js"
+  }
+]);
